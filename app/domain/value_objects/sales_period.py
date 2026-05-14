c02ac9c0e6d074c374dd2231876f513106da62af
mlwy4id <azmiimaulawiysaid67@gmail.com>
2026-05-15 06:51:11 +0800

    feat: add new method at saled period object value

--- app/domain/value_objects/sales_period.py
+++ app/domain/value_objects/sales_period.py
@@ -10,3 +10,12 @@ class SalesPeriod:
     def __post_init__(self):
         if self.end_date <= self.start_date:
             raise ValueError("End date must be after start date")
+
+    def is_coming_soon(self) -> bool:
+        return self.start_date > date.today()
+
+    def is_ended(self) -> bool:
+        return self.end_date < date.today()
+
+    def is_active(self) -> bool:
+        return self.start_date <= date.today() <= self.end_date
