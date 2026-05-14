c320cff49ff353be384bb301c9f770259a5109f8
mlwy4id <azmiimaulawiysaid67@gmail.com>
2026-05-15 06:38:00 +0800

    feat: add ticket entity

---
+++ app/domain/entities/ticket.py
@@ -0,0 +1,20 @@
+import uuid
+
+from app.domain.value_objects.booking_id import BookingId
+from app.domain.value_objects.event_id import EventId
+from app.domain.value_objects.ticket_code import TicketCode
+from app.domain.value_objects.ticket_id import TicketId
+from app.domain.value_objects.ticket_status import TicketStatus
+
+
+class Ticket:
+    def __init__(
+        self,
+        booking_id: BookingId,
+        event_id: EventId,
+    ):
+        self.id = TicketId(str(uuid.uuid4()))
+        self.ticket_code = TicketCode(str(uuid.uuid4()))
+        self.booking_id = booking_id
+        self.event_id = event_id
+        self.status = TicketStatus.ACTIVE
