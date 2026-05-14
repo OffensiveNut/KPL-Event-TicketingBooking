2ec604f84a4a731a4c81e40ab9e440a8d4896ff9
mlwy4id <azmiimaulawiysaid67@gmail.com>
2026-05-14 12:19:37 +0800

    feat: add SalesPeriod eq method

--- app/domain/value_objects/sales_period.py
+++ app/domain/value_objects/sales_period.py
@@ -8,3 +8,8 @@ class SalesPeriod:
 
         self.start_date = start_date
         self.end_date = end_date
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, SalesPeriod):
+            return False
+        return self.start_date == other.start_date and self.end_date == other.end_date
